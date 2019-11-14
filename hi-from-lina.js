5b49abfc71b85e93bcf9acd679e9125515a89792
sikessle <simon.kessler@icloud.com>
2019-11-14 10:20:40 +0100

    Initial

---
+++ hi-from-lina.js
@@ -0,0 +1,7 @@
+var worldArchive = [];
+
+function preserveForFutureGenerations(message) {
+  worldArchive.push(message);
+}
+
+preserveForFutureGenerations("Hi, this is Lina from Switzerland! I was born in 2019");
